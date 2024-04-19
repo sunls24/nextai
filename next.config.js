4cc5c32df867fae7f294575a1d381ca33107610a
sunls24 <sunls24@outlook.com>
2024-04-19 14:56:51 +0800

    Update dependencies & next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/api/x/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,POST",
-          },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Max-Age", value: "86400" },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
